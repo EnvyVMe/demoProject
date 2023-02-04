267286128457a42c9bd82ee53e3091c0f4dbf2b1
Alek Suendermann <Alek.Suendermann@stud.tu-darmstadt.de>
2023-02-04 16:38:47 +0100

    Added User, Repository, Controller and Bootstrapper

---
+++ src/main/java/com/example/demoProject/User.java
@@ -0,0 +1,30 @@
+package com.example.demoProject;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private final String name;
+    private final String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
